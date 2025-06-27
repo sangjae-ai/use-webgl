dc4375b56ea01b0f9c47971a07d04bec23bcfacb
sangjae-ai <sangjae-ai@gmail.com>
2025-06-27 14:12:49 +0000

    [Modify] run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --enable_gpu --output screenshot_gpu.png
+__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia python main.py --enable_gpu --output screenshot_gpu.png
